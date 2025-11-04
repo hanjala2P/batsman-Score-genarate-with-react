11fda8ba580447f034f9c29fe52cc24ef57f911a
hanjala2p <mdhanjala016pc@gmail.com>
2025-11-04 12:40:00 +0600

    learn suspane

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,21 @@
 import Counter from './counter';
 import Batsman from './Batsman';
-import './App.css'
+import Users from './Users';
+import Friends from './Friends';
+import './App.css';
+import { Suspense } from 'react';
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res=>res.json());
+
+const fetchFriends= async()=>{
+  const res= await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+}
+
 
 function App() {
+  const friendsPromise= fetchFriends();
   function handleClick(){
     alert('i am clicked')
   }
@@ -15,11 +28,18 @@ function App() {
     <>
       
       <h1>Vite + React</h1>
+      <Suspense fallback={<h3>Friends are comming...</h3>}>
+          <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+      
+      <Suspense fallback={<h3>Loading...</h3>}>
 
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense>
       <button onClick={handleClick}>click me</button>
       <button onClick={()=>alert('ooooh mg')}>click me</button>
       <button onClick={()=> handleAdd5(8)}>click me</button>
-
+      
        <Batsman></Batsman>
       <Counter></Counter>
      
